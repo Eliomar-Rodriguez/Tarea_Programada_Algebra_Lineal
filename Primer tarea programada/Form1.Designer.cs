32ffac9d4faec39db2e38015ced3fccd1f4b76a5
Lazuli26 <alberthsalascalero@gmail.com>
2017-02-15 19:38:34 -0600

    ningun cambio importante

--- Primer tarea programada/Form1.Designer.cs
+++ Primer tarea programada/Form1.Designer.cs
@@ -36,23 +36,26 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.a11 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // txtFilas
             // 
             this.txtFilas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtFilas.Location = new System.Drawing.Point(270, 92);
+            this.txtFilas.Location = new System.Drawing.Point(180, 60);
+            this.txtFilas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtFilas.Name = "txtFilas";
-            this.txtFilas.Size = new System.Drawing.Size(100, 35);
+            this.txtFilas.Size = new System.Drawing.Size(68, 26);
             this.txtFilas.TabIndex = 0;
             // 
             // textBox2
             // 
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(386, 92);
+            this.textBox2.Location = new System.Drawing.Point(257, 60);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(37, 32);
+            this.textBox2.Size = new System.Drawing.Size(25, 22);
             this.textBox2.TabIndex = 1;
             this.textBox2.Text = "X";
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
@@ -60,9 +63,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(552, 75);
+            this.button1.Location = new System.Drawing.Point(368, 49);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 68);
+            this.button1.Size = new System.Drawing.Size(76, 44);
             this.button1.TabIndex = 2;
             this.button1.Text = "Generar matriz";
             this.button1.UseVisualStyleBackColor = true;
@@ -71,51 +75,65 @@
             // txtColumnas
             // 
             this.txtColumnas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtColumnas.Location = new System.Drawing.Point(429, 92);
+            this.txtColumnas.Location = new System.Drawing.Point(286, 60);
+            this.txtColumnas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtColumnas.Name = "txtColumnas";
-            this.txtColumnas.Size = new System.Drawing.Size(100, 35);
+            this.txtColumnas.Size = new System.Drawing.Size(68, 26);
             this.txtColumnas.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(22, 92);
+            this.label1.Location = new System.Drawing.Point(15, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(206, 29);
+            this.label1.Size = new System.Drawing.Size(136, 20);
             this.label1.TabIndex = 4;
             this.label1.Text = "Tamaño de matriz";
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(121, 162);
+            this.panel1.Location = new System.Drawing.Point(81, 105);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(632, 395);
+            this.panel1.Size = new System.Drawing.Size(421, 257);
             this.panel1.TabIndex = 5;
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(812, 162);
+            this.textBox1.Location = new System.Drawing.Point(541, 105);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 35);
+            this.textBox1.Size = new System.Drawing.Size(68, 26);
             this.textBox1.TabIndex = 7;
             // 
             // textBox3
             // 
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(821, 128);
+            this.textBox3.Location = new System.Drawing.Point(547, 83);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(82, 28);
+            this.textBox3.Size = new System.Drawing.Size(55, 19);
             this.textBox3.TabIndex = 6;
             this.textBox3.Text = "Valor";
             // 
+            // a11
+            // 
+            this.a11.Location = new System.Drawing.Point(33, 336);
+            this.a11.Name = "a11";
+            this.a11.Size = new System.Drawing.Size(100, 20);
+            this.a11.TabIndex = 8;
+            this.a11.Text = "a11";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(942, 615);
+            this.ClientSize = new System.Drawing.Size(628, 400);
+            this.Controls.Add(this.a11);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.panel1);
@@ -124,6 +142,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.txtFilas);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -141,6 +160,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox a11;
     }
 }
 
--- Primer tarea programada/Program.cs
+++ Primer tarea programada/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.
 namespace Primer_tarea_programada
 {
     static class Program
@@ -17,6 +17,17 @@ namespace Primer_tarea_programada
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            List<List<List<System.Windows.Forms.TextBox>>> ListaMatrices = new List<List<List<System.Windows.Forms.TextBox>>>();
+            for (int x = 0; x < 3; x++)
+            {
+                ListaMatrices.Add(new List<List<System.Windows.Forms.TextBox>>());
+                for (int y = 0; y < 5; y++)
+                {
+                    ListaMatrices[x].Add(new List<System.Windows.Forms.TextBox>());
+                }
+            }
+            string hola;
+            
         }
     }
 }
